b659febb7c76fb4ca3d2387cde3136db387f7348
yassine-mabrouk <mabroukyassine70@gmail.com>
2022-07-18 13:32:46 +0200

    testing

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Java Streams API
-
+## Strems 
+Introduced in Java 8, the Stream API is used to process collections of objects. A stream is a sequence of objects that supports various methods which can be pipelined to produce the desired result.
+The features of Java stream are:
+- A stream is not a data structure instead it takes input from the Collections, Arrays or I/O channels.
+- Streams don’t change the original data structure, they only provide the result as per the pipelined methods.
+- Each intermediate operation is lazily executed and returns a stream as a result, hence various intermediate operations can be pipelined. Terminal operations mark the end of the stream and return the result.
+## Main topics 
 - What is functional programming
 - Stream API
 - Transformations with Map
--- src/test/java/com/amigoscode/examples/GettingStarted.java
+++ src/test/java/com/amigoscode/examples/GettingStarted.java
@@ -17,10 +17,26 @@ public class GettingStarted {
         // 1. Find people aged less or equal 18
         // 2. Then change implementation to find first 10 people
         List<Person> people = MockData.getPeople();
+        List<Person> result = new ArrayList<>();
+
+        int c=0;
+        for (Person p: people
+        ) {
+            if(p.getAge()  < 18 ){
+                result.add(p);
+                c++;
+                if (c >10 ) break;
+            }
+        }
+        result.forEach(System.out::println);
     }
+
 
     @Test
     public void declarativeApproachUsingStreams() throws Exception {
         List<Person> people = MockData.getPeople();
+        List<Person> result = new ArrayList<>();
+        result=  people.stream().filter(p -> p.getAge() < 5).limit(3).collect(Collectors.toList());
+        result.forEach(System.out::println);
     }
 }
